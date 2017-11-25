17b68bf0544ce5a17ad5d19ab75408b78f806e69
Giuseppe <toxiicpp@gmail.com>
2017-11-25 17:45:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 IP=$1 # 1st parameter, ip fail over
-ROUTE=$2 # 2st paramter, route gateway (generically the dedicated server ending with .254 or .1)
+ROUTE=$2 # 2th paramter, route gateway (generally the dedicated server ending with .254 or .1)
 echo -e "auto lo\niface lo inet loopback\n\nauto eth0\niface eth0 inet static\n\taddress $IP\n\tnetmask 255.255.255.0\n\tbroadcast $IP\n\tpost-up route add $ROUTE dev eth0\n\tpost-up route add default gw $ROUTE\n\tpre-down route del $ROUTE dev eth0\n\tpre-down route del default gw $ROUTE" > /etc/network/interfaces
 ifup eth0
 service networking restart
